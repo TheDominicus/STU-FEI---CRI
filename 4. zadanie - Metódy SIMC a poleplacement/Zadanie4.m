f0b72b8dad11226a1c779d3ccd277c08b03cfcdc
Dominik Šuráni <xsurani@stuba.sk>
2024-03-25 11:26:35 +0100

    Update Zadanie4.m

--- 4. zadanie - Metódy SIMC a poleplacement/Zadanie4.m
+++ 4. zadanie - Metódy SIMC a poleplacement/Zadanie4.m
@@ -7,9 +7,15 @@ T = 300;
 T1 = 935;
 T2 = 25;
 Delay = 300;
-Beta = 1.944;
 
 citatel = (K/(T1*T2));
 menovatel = [1,(T1+T2)/(T1*T2), 1/(T1*T2)];
 
-%% 
+%% SIMC
+
+
+%% Poleplacement
+
+
+%% Simulácia a výpis
+out = sim("BlokSchema.slx");
